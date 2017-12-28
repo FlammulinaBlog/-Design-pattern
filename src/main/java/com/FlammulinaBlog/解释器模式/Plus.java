c78f1cee252f207a0a4d4565c5786c7712efa318
FlammulinaBlog <FlammulinaBlog@163.com>
2017-12-28 09:29:22 +0800

    解释器模式

---
+++ src/main/java/com/FlammulinaBlog/解释器模式/Plus.java
@@ -0,0 +1,8 @@
+package com.FlammulinaBlog.解释器模式;
+
+public class Plus implements Expression {
+    @Override
+    public int interpret(Context context) {
+        return context.getNum1()+context.getNum2();
+    }
+}
